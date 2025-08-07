03db64db8abf461177a817d512ba8b0fa2af03a4
LEO <130946664+XxLEOCR22xX@users.noreply.github.com>
2025-08-07 14:11:30 -0400

    Add files via upload

---
+++ Assets/scrip/Rotacion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+
+    // Update is called once per frame
+    
+}
